7b5bcbbafd14d8a2e86d131e84f39a4a378fed52
Justin Liman <justinliman97@gmail.com>
2021-10-27 21:08:58 -0700

    create component files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import React from 'react';
 import './App.css';
+import Navigation from './Navigation';
+import Logo from './Logo';
+import ImageLinkForm from './ImageLinkForm';
+import FaceRecognition from './FaceRecognition';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
+      <Logo />
+      <ImageLinkForm />
+      <FaceRecognition />
     </div>
   );
 }
---
+++ src/FaceRecognition.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FaceRecognition = () => {
+    return <h1>FaceRecognition</h1>;
+}
+
+export default FaceRecognition;
