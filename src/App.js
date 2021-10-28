b5d16a004756b9d74eed2a901524ec35b1edd7e7
Justin Liman <justinliman97@gmail.com>
2021-10-27 21:16:05 -0700

    move components in to separate folders

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
-import Navigation from './Navigation';
-import Logo from './Logo';
-import ImageLinkForm from './ImageLinkForm';
-import FaceRecognition from './FaceRecognition';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <Logo />
+      {/* <Logo />
       <ImageLinkForm />
-      <FaceRecognition />
+      <FaceRecognition /> */}
     </div>
   );
 }
